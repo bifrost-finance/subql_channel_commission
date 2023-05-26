6e9711b8555171473566542d79dfab2602c3241f
herryho <herry.heyi@gmail.com>
2023-05-26 14:42:10 +0800

    fix handleVtokenMintingMinted

--- src/mappings/SlpMintingHandlers.ts
+++ src/mappings/SlpMintingHandlers.ts
@@ -23,33 +23,36 @@ export async function handleVtokenMintingMinted(
   // 默认channelCode是Bifrost
   let channelCode =
     "0x2597b02db29adca18d6f70a494955f58f2a11895b0c3873d71db5172be390f99";
-  // 先看看这个mint是不是带渠道的
-  let extrinsicEvents = event.extrinsic.events;
-  // 看看这个extrinsic的event数组里有没有Minted事件和remark事件。因为这是从Minted事件进去的，所以只需要找有没有remark事件就好
-  for (const extrinsicEvent of extrinsicEvents) {
-    if (extrinsicEvent.event.index.toString() == "0x0005") {
-      const [fromAccount, forChannelCode] = extrinsicEvent.event.data;
 
-      if (address.toString() == fromAccount.toString()) {
-        channelCode = `${forChannelCode.toString()}`;
-        logger.info(`remark事件: ${JSON.stringify(extrinsicEvent)}`);
+  if (event.extrinsic) {
+    // 先看看这个mint是不是带渠道的
+    let extrinsicEvents = event.extrinsic.events;
+    // 看看这个extrinsic的event数组里有没有Minted事件和remark事件。因为这是从Minted事件进去的，所以只需要找有没有remark事件就好
+    for (const extrinsicEvent of extrinsicEvents) {
+      if (extrinsicEvent.event.index.toString() == "0x0005") {
+        const [fromAccount, forChannelCode] = extrinsicEvent.event.data;
+
+        if (address.toString() == fromAccount.toString()) {
+          channelCode = `${forChannelCode.toString()}`;
+          logger.info(`remark事件: ${JSON.stringify(extrinsicEvent)}`);
+        }
       }
+
+      // token type
+      const tokenName = await getTokenName(currencyId);
+      const account = (address as AccountId).toString();
+      const amount = (vtokenAmount as Balance).toString();
+
+      record.event = "Mint";
+      record.account = account;
+      record.vtokenId = `V${tokenName.toUpperCase()}`;
+      record.amount = amount;
+      record.blockHeight = blockNumber;
+      record.timestamp = Math.floor(event.block.timestamp.getTime() / 1000);
+      record.channelCode = channelCode;
+
+      await record.save();
     }
-
-    // token type
-    const tokenName = await getTokenName(currencyId);
-    const account = (address as AccountId).toString();
-    const amount = (vtokenAmount as Balance).toString();
-
-    record.event = "Mint";
-    record.account = account;
-    record.vtokenId = `V${tokenName.toUpperCase()}`;
-    record.amount = amount;
-    record.blockHeight = blockNumber;
-    record.timestamp = Math.floor(event.block.timestamp.getTime() / 1000);
-    record.channelCode = channelCode;
-
-    await record.save();
   }
 }
 
